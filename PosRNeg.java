4ede3c7a00d305e67bd831a403678c9cf2b39566
deepk0328 <deepakpvp@gmail.com>
2019-03-19 22:35:10 +0530

    positive r negative

---
+++ PosRNeg.java
@@ -0,0 +1,12 @@
+impoert java.util.Scanner;
+
+class PosRNeg{
+	public static void main(String args[]){
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		if(num >0)
+			System.out.println("Positive");
+		if(num<0)
+			System.out.println("Negative");
+		if(num==0)
+			System.out.println("Zero");
